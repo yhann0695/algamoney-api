cdcdcd7ebb9fab3aee4775f1b21b4b2e90964c52
Yhann Carvalho <yhann0695@hotmail.com>
2021-01-21 14:53:27 -0300

    Inicio projeto back-end

---
+++ back-end/algamoney-api/src/main/java/com/algaworks/algamoneyapi/AlgamoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algamoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamoneyApiApplication.class, args);
+	}
+
+}
