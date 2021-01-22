c41626b51f35b5966527b349e06ad43579206034
Yhann Carvalho <yhann0695@hotmail.com>
2021-01-21 22:48:12 -0300

    "criação do modelo categoria"

---
+++ back-end/algamoney-api/src/main/java/com/algaworks/algamoneyapi/domain/model/Categoria.java
@@ -0,0 +1,25 @@
+package com.algaworks.algamoneyapi.domain.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "TB_CATEGORIA")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Categoria implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @Column(name = "CO_CATEGORIA")
+    private Long codigo;
+
+    @Column(name = "NO_CATEGORIA")
+    private String nome;
+}
