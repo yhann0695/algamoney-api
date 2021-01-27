dacec7c6c3ac623d5cc6026bae640f8998b658d8
Yhann Carvalho <yhann0695@hotmail.com>
2021-01-27 17:38:54 -0300

    Criação do formulário de pesquisa

--- front-end/algamoney-ui/src/app/app.module.ts
+++ front-end/algamoney-ui/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
